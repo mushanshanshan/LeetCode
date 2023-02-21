0929b22d50aabc791c42c9d8b41d1d9a81305bc5
mushanshanshan <wwd137669793@gmail.com>
2023-02-21 14:17:37 +1100

    .

---
+++ 540. Single Element in a Sorted Array 2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int l = 0, r = nums.size() - 1;
+
+        while (l < r) {
+            int m = l + (r - l) / 2;
+
+            if (nums[m] == nums[m ^ 1]) l = m + 1;
+            else r = m;
+
+        }
+
+        return nums[l];
+    }
+};
